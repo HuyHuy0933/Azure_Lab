41f890234e8912924a712adf2b0b824955f0db4c
HuyHuy0933 <40488506+HuyHuy0933@users.noreply.github.com>
2020-11-05 07:59:16 +0700

    monitor

---
+++ Monitor/Lab_188/az203-core-redis/az203-core/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace az203_core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal price { get; set; }
+    }
+}
