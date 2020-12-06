e275fbf728bf6583d8aaa0afe465ef1a95a72b06
HuyHuy0933 <40488506+HuyHuy0933@users.noreply.github.com>
2020-12-06 19:06:07 +0700

    new azure lab

---
+++ Monitor/Lab-192/az203-core/az203-core/Models/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace az203_core.Models
+{
+    public class ProductContext : DbContext
+    {
+        // Replace the database connection string  here
+        readonly string connectionstring = "Server=tcp:huyxle.database.windows.net,1433;Initial Catalog=huyxledb;Persist Security Info=False;User ID=huyxle;Password=Huy@0933950159;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connectionstring);
+            base.OnConfiguring(options);
+        }
+
+    }
+}
